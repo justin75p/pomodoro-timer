23cd41bd0424405e51cf177615e855de2cadcd21
Justin <justin75p@gmail.com>
2025-02-18 13:59:14 -0800

    begun working on button functions

---
+++ script.js
@@ -0,0 +1,20 @@
+let startButton = document.getElementById("start");
+let pauseButton = document.getElementById("pause");
+let resetButton = document.getElementById("reset");
+let timer = document.getElementById("timer");
+
+function startTimer() {
+
+}
+
+function pauseTimer() {
+
+}
+
+function resetTimer() {
+
+}
+
+startButton.addEventListener(startTimer);
+pauseButton.addEventListener(pauseTimer);
+resetButton.addEventListener(resetButton);
